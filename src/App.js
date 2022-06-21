2c4fb24873dc4070030c209fd47e10cd30fabb6d
Pranjal BHaskar <97526424+pranjalbhaskar-27@users.noreply.github.com>
2022-06-22 01:48:37 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  
+  const [count,setCount]=useState(0)
+  useEffect=()=>{
+    console.log(count)
+  }
+
+  const add1=()=>{
+    setCount(count+1)
+    document.getElementById('kuchhbhi').disabled=false;
+  }
+  const sub1=()=>{
+      if(count>0){
+        setCount(count-1)
+      }
+      else{
+        document.getElementById('kuchhbhi').disabled=true;
+      }
+  }
+  
+  
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{count}</h2>
+      <button data-testid="counter-decrement-button" id='kuchhbhi' onClick={sub1}>Sub 1</button>
+      <button data-testid="counter-increment-button" onClick={add1}>Add 1</button>
     </div>
   );
 }
